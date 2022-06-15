68568ed2029f083a3324ebbdc73f4e99d8062ea6
Martin Silva <silvabrunomartin@hotmail.com>
2022-06-14 21:09:35 -0300

    cartview

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,16 +1,24 @@
 import { createContext, useState } from 'react';
+import { Comida } from '../utils/productsMock';
 
 const CartContext = createContext()
 
 const CartProvider = ({chidren}) => {
     const [cartListItem , setCartListItem] = useState([])
+    const [totalPrice, setTotalPrice] = useState(0)
+
     const addProductCart = (comida) =>{
-        setCartListItem(cartListItem =>[...cartListItem, comida])
+        let isInCart = cartListItem.find(cartItem => cartItem.id === Comida.id)
+        if(!isInCart) {
+            setTotalPrice(totalPrice + comida.price)
+            return setCartListItem(cartListItem =>[...cartListItem, comida])
+        }
     }
 
     const data = {
         cartListItem,
-        addProductCart
+        addProductCart,
+        totalPrice
     }
 
     return(
--- src/utils/productsMock.js
+++ src/utils/productsMock.js
@@ -20,21 +20,25 @@ const Comida =
    [ {
         title : 'Hamburguesa con papas',
         image : 'food nro1.jpg',
+        price: 560,
         id:4
     },
     {
         title : 'Mix de carne con ensalada',
         image : 'food nro2.jpg',
+        price: 720,
         id:5
     },
     {
         title : 'Pizza Muzarella especial',
         image : 'food nro3.jpg',
+        price: 690,
         id:6
     },
     {
         title : 'Hamburguesa de cordero con pollo',
         image : 'food nro4.jpg',
+        price: 600,
         id:7
     }]
 
